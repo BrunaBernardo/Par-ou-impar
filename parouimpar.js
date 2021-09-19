9cd3d61bb5851b032d97e9669974f4cf60506c69
BrunaBernardo <89536152+BrunaBernardo@users.noreply.github.com>
2021-09-19 15:32:05 -0300

    Add files via upload

---
+++ parouimpar.js
@@ -0,0 +1,19 @@
+//par ou impar.
+
+
+ var Estudantes
+ var sala = 22
+
+
+    for ( var Estudantes =0; Estudantes <= sala; Estudantes++){ 
+
+     if(Estudantes %2 ==0){
+        if(Estudantes ==0){
+            console.log(" O numero " + Estudantes + " É Zero ");
+        }else{
+            console.log(" O numero " + Estudantes + " É Par ");
+        }
+         }else{
+        console.log(" O numero " + Estudantes + " É Impar ")
+      }
+}
